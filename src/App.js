252f6b89c9c56b2f5442c4d5c37acbc2f276d193
Jeongdong <59277907+poplar0713@users.noreply.github.com>
2023-10-10 16:49:47 +0900

    Make todo submit button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { TextField } from '@mui/material';
+import { Button } from '@mui/material';
+
+const TodoItemInputField = (props) => {
+  const [input, setInput] = useState("");
+
+  const onSubmit = () => {
+    props.onSubmit(input);
+    setInput("");
+  };
+
+  return (
+    <div>
+      <TextField
+        id = "todo-item-input"
+        label="TO DO"
+        variant='outlined'
+        onChange={(e) => setInput(e.target.value)} value={input}
+      />
+      <Button variant='outlined' onClick={onSubmit}> 등록 </Button>
+    </div>
+  );
+}
+
+const TodoItemList = (props) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoItemInputField onSubmit={(input) => {
+        console.log('submit ' + input);
+      }}/>
+      <TodoItemList />
     </div>
   );
 }
 
 export default App;
+ 
